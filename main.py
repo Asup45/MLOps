3f93267e93730abea3081d0939a18ffcfc627c22
Asup <thibaut.dessolin@gmail.com>
2024-04-22 14:39:26 +0200

    added Jean repository

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from mlflow.models import infer_signature
 import pandas as pd
 
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
 from sklearn.metrics import r2_score, mean_squared_error
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
@@ -29,7 +29,7 @@ preprocessor = ColumnTransformer(
 
 # Création du pipeline
 pipeline = Pipeline(steps=[('preprocessor', preprocessor),
-                           ('regressor', RandomForestRegressor())])
+                           ('regressor', XGBRegressor())])
 
 # On définie les données explicative et la target
 X = df.drop('salary_in_usd', axis=1)
@@ -38,9 +38,9 @@ y = df['salary_in_usd']
 # La grille de paramètres que notre modèle utiliseras
 params = {
     "regressor__n_estimators": [200, 300, 500],
-    "regressor__max_depth": [30, 40, 50],
-    "regressor__random_state": [8888],
-    "regressor__max_features": ["sqrt"],
+    "regressor__max_depth": [6, 8, 10],
+    "regressor__learning_rate": [0.01, 0.05, 0.1],
+    "regressor__random_state": [8888]
 }
 
 grid_search = GridSearchCV(pipeline, param_grid=params, cv=5, scoring='neg_mean_squared_error', verbose=0)
@@ -73,18 +73,17 @@ with mlflow.start_run():
     mlflow.log_metric("r2", r2)
     mlflow.log_metric("MSE", MSE)
 
-    mlflow.set_tag("Training Info", "Random Forest Regressor model for ds_salaries data")
+    mlflow.set_tag("Training Info", "XGBoost Regressor model for ds_salaries data")
 
     signature = infer_signature(X_train, best_model.predict(X_train))
 
     model_info = mlflow.sklearn.log_model(
     sk_model=best_model,
-    artifact_path="RandomForestRegressor_model",
+    artifact_path="XGBRegressor_model",
     signature=signature,
     input_example=X_train,
-    registered_model_name="tracking-quickstart",
+    registered_model_name="tracking-XGBoost",
 )
-
 
 loaded_model = mlflow.pyfunc.load_model(model_info.model_uri)
 
