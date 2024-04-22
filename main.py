bef789efb4f9fba189afc702b7ac03840b4a7716
Asup <thibaut.dessolin@gmail.com>
2024-04-22 14:12:41 +0200

    added RandomForestRegressor model

--- main.py
+++ main.py
@@ -1,58 +1,85 @@
+# Import des librairies
 import mlflow
 import mlflow.pyfunc
 from mlflow.models import infer_signature
 import pandas as pd
 
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
-mlflow.set_tracking_uri("http://localhost:5000")
+# On charge les données
+df = pd.read_csv("ds_salaries.csv")
 
-X, y = datasets.load_iris(return_X_y=True)
+# Prétraitment des données
+# Définition des variables catégorielles et numérique
+numerical_features = ["work_year", "salary", "remote_ratio"]
+categorial_features = ["experience_level", "employment_type", "job_title", "salary_currency", 
+                       "employee_residence", "company_location", "company_size"]
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
+# Création du préprocesseur
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', StandardScaler(), numerical_features),
+        ('cat', OneHotEncoder(drop='first', handle_unknown='ignore'), categorial_features)
+    ])
 
+# Création du pipeline
+pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+                           ('regressor', RandomForestRegressor())])
+
+# On définie les données explicative et la target
+X = df.drop('salary_in_usd', axis=1)
+y = df['salary_in_usd']
+
+# La grille de paramètres que notre modèle utiliseras
 params = {
-    "solver": "lbfgs",
-    "max_iter": 1000,
-    "multi_class": "auto",
-    "random_state": 8888,
+    "regressor__n_estimators": [200, 300, 500],
+    "regressor__max_depth": [30, 40, 50],
+    "regressor__random_state": [8888],
+    "regressor__max_features": ["sqrt"],
 }
 
-lr = LogisticRegression(**params)
-lr.fit(X_train, y_train)
+grid_search = GridSearchCV(pipeline, param_grid=params, cv=5, scoring='neg_mean_squared_error', verbose=0)
 
-y_pred = lr.predict(X_test)
+# On divise les données en jeu d'entrainement et de validation
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-accuracy = accuracy_score(y_test, y_pred)
-precision = precision_score(y_test, y_pred, average='macro')
-recall = recall_score(y_test, y_pred, average='macro')
-f1 = f1_score(y_test, y_pred, average='macro')
+# On entraine le modèle
+grid_search.fit(X_train, y_train)
 
+# Meilleur modèle
+best_model = grid_search.best_estimator_
+
+# On fait les prédictions sur l'ensemble de test
+y_pred = best_model.predict(X_test)
+
+# Métriques d'évaluation pour notre modèle
+r2 = r2_score(y_test, y_pred)
+MSE = mean_squared_error(y_test, y_pred)
+
+# Utilisation de MLFlow
 mlflow.set_tracking_uri("http://localhost:5000")
 
-mlflow.set_experiment("MLflow Quickstart")
+mlflow.set_experiment("Salaries Prediction")
 
 with mlflow.start_run():
 
     mlflow.log_params(params)
 
-    mlflow.log_metric("accuracy", accuracy)
-    mlflow.log_metric("precision", precision)
-    mlflow.log_metric("recall", recall)
-    mlflow.log_metric("f1 score", f1)
+    mlflow.log_metric("r2", r2)
+    mlflow.log_metric("MSE", MSE)
 
-    mlflow.set_tag("Training Info", "Basic LR model for iris data")
+    mlflow.set_tag("Training Info", "Random Forest Regressor model for ds_salaries data")
 
-    signature = infer_signature(X_train, lr.predict(X_train))
+    signature = infer_signature(X_train, best_model.predict(X_train))
 
     model_info = mlflow.sklearn.log_model(
-    sk_model=lr,
-    artifact_path="iris_model",
+    sk_model=best_model,
+    artifact_path="RandomForestRegressor_model",
     signature=signature,
     input_example=X_train,
     registered_model_name="tracking-quickstart",
@@ -63,9 +90,10 @@ loaded_model = mlflow.pyfunc.load_model(model_info.model_uri)
 
 predictions = loaded_model.predict(X_test)
 
-iris_feature_names = datasets.load_iris().feature_names
+ohe_categories = best_model.named_steps['preprocessor'].named_transformers_['cat'].get_feature_names_out(input_features=categorial_features)
+feature_names = list(ohe_categories) + numerical_features
 
-result = pd.DataFrame(X_test, columns=iris_feature_names)
+result = pd.DataFrame(X_test, columns=feature_names)
 result["actual class"] = y_test
 result["predicted class"] = predictions
 
