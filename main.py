67f97fc1f239d56e5d4723a68e4f4a0a430682bc
Asup <thibaut.dessolin@gmail.com>
2024-04-23 12:01:46 +0200

    XGBoost modification

--- main.py
+++ main.py
@@ -14,11 +14,50 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 # On charge les données
 df = pd.read_csv("ds_salaries.csv")
 
-# Prétraitment des données
+# Filtrer les devises à USD, EUR et GBP
+df = df[df['salary_currency'].isin(['USD', 'EUR', 'GBP'])]
+
+# Définir le taux de conversion
+conversion_rates = {
+    'USD': 0.94,
+    'GBP': 1.14,
+    'EUR': 1.0,
+    'INR': 0.011,
+    'CAD': 0.70,
+    'AUD': 0.64,
+    'SGD': 0.71,
+    'BRL': 0.18,
+    'PLN': 0.22,
+    'CHF': 1.01,
+    'HUF': 0.0027,
+    'DKK': 0.13,
+    'JPY': 0.0073,
+    'TRY': 0.051,
+    'THB': 0.027,
+    'ILS': 0.26,
+    'HKD': 0.12,
+    'CZK': 0.041,
+    'MXN': 0.052,
+    'CLP': 0.0011
+}
+
+# Convertir le salaire à EUR
+df['salary_eur'] = df.apply(lambda row: row['salary'] * conversion_rates[row['salary_currency']], axis=1)
+
+# Fonction pour obtenir la plage salariale
+def get_salary_range(salary):
+    return round(int(salary / 10000), 1)
+
+# Appliquer la fonction à la colonne 'salary_eur'
+df['salary_range'] = df['salary_eur'].apply(get_salary_range)
+
+# Supprimer les colonnes inutiles
+df = df.drop(columns=['salary', 'salary_currency', 'salary_in_usd', 'salary_eur'])
+
 # Définition des variables catégorielles et numérique
-numerical_features = ["work_year", "salary", "remote_ratio"]
-categorial_features = ["experience_level", "employment_type", "job_title", "salary_currency", 
-                       "employee_residence", "company_location", "company_size"]
+numerical_features = ["work_year", "remote_ratio"]
+categorial_features = ['experience_level', 'employment_type', 'job_title', 
+                       'employee_residence', 'company_location', 'company_size']
 
 # Création du préprocesseur
 preprocessor = ColumnTransformer(
@@ -32,18 +71,18 @@ pipeline = Pipeline(steps=[('preprocessor', preprocessor),
                            ('regressor', XGBRegressor())])
 
 # On définie les données explicative et la target
-X = df.drop('salary_in_usd', axis=1)
-y = df['salary_in_usd']
+X = df.drop('salary_range', axis=1)
+y = df['salary_range']
 
 # La grille de paramètres que notre modèle utiliseras
 params = {
-    "regressor__n_estimators": [200, 300, 500],
-    "regressor__max_depth": [6, 8, 10],
-    "regressor__learning_rate": [0.01, 0.05, 0.1],
+    "regressor__n_estimators": [200, 300, 500, 1000],
+    "regressor__max_depth": [8, 10, 20],
+    "regressor__learning_rate": [0.001, 0.01, 0.05, 0.1],
     "regressor__random_state": [8888]
 }
 
-grid_search = GridSearchCV(pipeline, param_grid=params, cv=5, n_jobs=-1, scoring='neg_mean_squared_error', verbose=0)
+grid_search = GridSearchCV(pipeline, param_grid=params, cv=5, n_jobs=-1, scoring='r2', verbose=0)
 
 # On divise les données en jeu d'entrainement et de validation
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
